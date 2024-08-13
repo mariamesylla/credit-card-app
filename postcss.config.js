2fa0e029e8cc922ae8bdd8bde1de90d924d160eb
Magnan Sylla <magnan.m.sylla@aexp.com>
2024-08-13 12:03:15 -0400

    build fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     plugins: {
       tailwindcss: { config: './tailwind.config.js' },
+    "autoprefixer": {},
+
+    "postcss-preset-env": {
+
+    "stage": 0
+
+    }
     },
   }
